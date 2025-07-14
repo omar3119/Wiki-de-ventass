0dc66f54b7969d8f4a6d20c8d467fa1df4b29877
Omar01~ <omarreyez31@gmail.com>
2025-07-14 13:40:54 -0500

    toggle theme icon between sun and moon

--- wiki-de-ventas/src/components/Header/Header.jsx
+++ wiki-de-ventas/src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
+//Icons
 import { FaMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa";
+
 import { useContext } from "react";
 import { context } from "../../context/context"; // Import the context
 import "../../styles/Header.scss"; // Import the styles for the Header component
 
 const Header = () => {
- const { toggleTheme } = useContext(context); 
+  const { toggleTheme, darkMode } = useContext(context);
+  console.log(darkMode);
 
   const navText = [
     { name: "Home", path: "/" },
@@ -24,7 +28,11 @@ const Header = () => {
           </ul>
         </nav>
         <div>
-          <FaMoon onClick={toggleTheme} className="moon-icon" />
+          {darkMode ? (
+            <FaMoon onClick={toggleTheme} className="icon_theme" />
+          ) : (
+            <FaSun onClick={toggleTheme} className="icon_theme" />
+          )}
         </div>
       </div>
     </header>
